3e751b80db6178fe956cf48c4aed137891005b37
sebastian897 <sebastian@schonrocks.com>
2026-03-03 16:19:35 +0000

    mager refactor

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 
 #include "raymath.h"
 
-
 #define ARRAY_LENGTH(x) (sizeof(x) / sizeof((x)[0]))
 
-#define max_envobjs 256
-enum { tile_size = 64 };
+#define map_max_width 256
+#define map_max_height 256
+enum { tile_size = 64, hallway_size = 2 };
 Texture2D wall_texture;
 Texture2D wall_corner_texture;
 Texture2D tile_texture;
@@ -20,19 +20,35 @@ typedef struct Player {
   float speed;
 } Player;
 
-typedef struct EnvObj {
-  Rectangle rec;
+typedef struct MapTile {
+  Vector2 pos;
   Texture2D texture;
   int rot;
   bool can_col;
-} EnvObj;
+} MapTile;
 
-typedef struct EnvObjs {
-  EnvObj arr[max_envobjs];
-  int count;
-} EnvObjs;
+typedef struct Map {
+  MapTile arr[map_max_height][map_max_width];
+} Map;
 
-Vector2 GetPosOfRec(Rectangle rec) {
+typedef struct MapPos {
+  int x;
+  int y;
+} MapPos;
+
+typedef struct ScreenPos {
+  int x;
+  int y;
+} ScreenPos;
+
+typedef struct Rec {
+  int x;
+  int y;
+  int width;
+  int height;
+} Rec;
+
+Vector2 GetPosOfRec(Rec rec) {
   return (Vector2){rec.x, rec.y};
 }
 
@@ -40,37 +56,35 @@ bool PBetween2P(float p, float p2l, float p2h) {
   return p >= p2l && p <= p2h;
 }
 
-bool Collided(Rectangle r1, Rectangle r2) {
-  return (PBetween2P(r1.x, r2.x, r2.x + r2.width) ||
-          PBetween2P(r1.x + r1.width, r2.x, r2.x + r2.width)) &&
-         (PBetween2P(r1.y, r2.y, r2.y + r2.height) ||
-          PBetween2P(r1.y + r1.height, r2.y, r2.y + r2.height));
+bool Collided(Vector2 r1, Vector2 r2) {
+  return (PBetween2P(r1.x, r2.x, r2.x + tile_size) ||
+          PBetween2P(r1.x + tile_size, r2.x, r2.x + tile_size)) &&
+         (PBetween2P(r1.y, r2.y, r2.y + tile_size) ||
+          PBetween2P(r1.y + tile_size, r2.y, r2.y + tile_size));
 }
 
-void MovePlayer(Player* p, Camera2D* c, EnvObjs* envobjs) {
+void MovePlayer(Player* p, Camera2D* c, Map* map) {
   float newx = p->pos.x + (IsKeyDown(KEY_D) - IsKeyDown(KEY_A)) * p->speed;
   float newy = p->pos.y + (IsKeyDown(KEY_S) - IsKeyDown(KEY_W)) * p->speed;
   bool xcol = false;
   bool ycol = false;
-  for (int i = 0; i < envobjs->count; i++) {
-    if (!envobjs->arr[i].can_col) continue;
-    if (Collided((Rectangle){newx, p->pos.y, p->width, p->height}, envobjs->arr[i].rec))
-      xcol = true;
-    if (Collided((Rectangle){p->pos.x, newy, p->width, p->height}, envobjs->arr[i].rec))
-      ycol = true;
+  for (int my = 0; my < map_max_height; my++) {
+    for (int mx = 0; mx < map_max_width; mx++) {
+      if (!map->arr[my][mx].can_col) continue;
+      if (Collided((Vector2){newx, p->pos.y}, map->arr[my][mx].pos)) xcol = true;
+      if (Collided((Vector2){p->pos.x, newy}, map->arr[my][mx].pos)) ycol = true;
+    }
   }
   if (!xcol) p->pos.x = newx;
   if (!ycol) p->pos.y = newy;
   c->target = p->pos;
 }
 
-void RecSqrs(EnvObjs* envobjs, Rectangle rec, Texture2D texture, float rot, bool can_col) {
+void RecSqrs(Map* map, Rec rec, Texture2D texture, float rot, bool can_col) {
   for (int y = 0; y < rec.height; y++) {
     for (int x = 0; x < rec.width; x++) {
-      envobjs->arr[envobjs->count] = (EnvObj){
-          (Rectangle){rec.x + (x * tile_size), rec.y + (y * tile_size), tile_size, tile_size},
-          texture, rot, can_col};
-      envobjs->count++;
+      map->arr[rec.x + x][rec.y + y] =
+          (MapTile){(Vector2){rec.x + x, rec.y + y}, texture, rot, can_col};
     }
   }
 }
@@ -80,43 +94,39 @@ int corner_texture[4] = {0, 1, 2, 3};
 int rx[4] = {0, 1, 1, 0};
 int ry[4] = {0, 0, 1, 1};
 
-void AddObjsToEnvObjs(EnvObjs* envobjs, EnvObjs newenvobjs) {
-  for (int o = 0; o < newenvobjs.count; o++) {
-    envobjs->arr[envobjs->count] = newenvobjs.arr[o];
-    envobjs->count++;
+void AddObjsToEnvObjs(Map* map, Map newenvobjs) {
+  for (int my = 0; my < map_max_height; my++) {
+    for (int mx = 0; mx < map_max_width; mx++) {
+      map->arr[mx][my] = newenvobjs.arr[mx][my];
+    }
+  }
+}
+void RotateEnvObjs(Map* map, Vector2 origin, int rot) {
+  for (int my = 0; my < map_max_height; my++) {
+    for (int mx = 0; mx < map_max_width; mx++) {
+      Vector2 new_pos = Vector2Rotate(Vector2Subtract(map->arr[mx][my].pos, origin), PI / 2 * rot);
+      map->arr[mx][my].pos = (Vector2){origin.x + new_pos.x,origin.y + new_pos.y};
+      map->arr[mx][my].rot = (map->arr[mx][my].rot + rot) % 4;
+    }
   }
 }
 
-void RotateEnvObjs(EnvObjs* envobjs, Vector2 origin, int rot) {
-  for (int o = 0; o < envobjs->count; o++) {
-    Vector2 new_pos =
-        Vector2Rotate((Vector2){envobjs->arr[o].rec.x - origin.x, envobjs->arr[o].rec.y - origin.y},
-                      PI / 2 * rot);
-    envobjs->arr[o].rec = (Rectangle){origin.x + new_pos.x, origin.y + new_pos.y,
-                                      envobjs->arr[o].rec.width, envobjs->arr[o].rec.height};
-    envobjs->arr[o].rot = (envobjs->arr[o].rot + rot) % 4;
-  }
+void Wall(Map* wallobjs, Rec rec) {
+  RecSqrs(wallobjs, (Rec){rec.x + tile_size, rec.y, rec.width, 1}, wall_texture, texture[0], true);
+  wallobjs->arr[rec.x][rec.y] =
+      (MapTile){(Vector2){rec.x, rec.y}, wall_corner_texture, corner_texture[0], true};
 }
 
-void Wall(EnvObjs* wallobjs, Rectangle rec) {
-  RecSqrs(wallobjs, (Rectangle){rec.x + tile_size, rec.y, rec.width, 1}, wall_texture, texture[0],
-          true);
-  wallobjs->arr[wallobjs->count] = (EnvObj){(Rectangle){rec.x, rec.y, tile_size, tile_size},
-                                            wall_corner_texture, corner_texture[0], true};
-  wallobjs->count++;
-}
-
-void CreateRoom(EnvObjs* envobjs, Rectangle rec) {
+void CreateRoom(Map* map, Rec rec) {
   for (int d = 0; d < 4; d++) {
-    EnvObjs new_wall_objs = {0};
-    Rectangle new_wall = {rec.x + (rx[d] * ((rec.width - 2) + 1)) * tile_size,
-                          rec.y + (ry[d] * ((rec.height - 2) + 1)) * tile_size, rec.width - 2, 1};
+    Map new_wall_objs = {0};
+    Rec new_wall = {rec.x + (rx[d] * ((rec.width - 2) + 1)),
+                    rec.y + (ry[d] * ((rec.height - 2) + 1)), rec.width - 2, 1};
     Wall(&new_wall_objs, new_wall);
     RotateEnvObjs(&new_wall_objs, GetPosOfRec(new_wall), d);
-    AddObjsToEnvObjs(envobjs, new_wall_objs);
+    AddObjsToEnvObjs(map, new_wall_objs);
   }
-  RecSqrs(envobjs,
-          (Rectangle){rec.x + tile_size, rec.y + tile_size, (rec.width - 2), (rec.height - 2)},
+  RecSqrs(map, (Rec){rec.x + tile_size, rec.y + tile_size, (rec.width - 2), (rec.height - 2)},
           tile_texture, 0, false);
 }
 
@@ -154,30 +164,32 @@ int main(void) {
   camera.rotation = 0.0f;
   camera.zoom = 1.0f;
 
-  EnvObjs envobjs = {0};
-  CreateRoom(&envobjs, (Rectangle){0, 0, 10, 10});
+  Map map = {0};
+  CreateRoom(&map, (Rec){0, 0, 10, 10});
 
-  // Wall(&envobjs, (Rectangle){0 , 0, 5,1});
-  // envobjs.arr[0] =
-  //     (EnvObj){(Rectangle){0 , 0, tile_size, tile_size},
+  // Wall(&map, (Rec){0 , 0, 5,1});
+  // map.arr[0] =
+  //     (MapTile){(Rec){0 , 0, tile_size, tile_size},
   //              wall_texture, 0, false};
-  //     envobjs.count++;
-  // RotateEnvObjs(&envobjs, (Vector2){0,0}, 1);
+  //     map.count++;
+  // RotateEnvObjs(&map, (Vector2){0,0}, 1);
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
-    MovePlayer(&player, &camera, &envobjs);
+    MovePlayer(&player, &camera, &map);
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
     BeginMode2D(camera);
 
-    for (int i = 0; i < envobjs.count; i++) {
-      DrawTexturePro(envobjs.arr[i].texture, (Rectangle){0, 0, tile_size, tile_size},
-                     (Rectangle){envobjs.arr[i].rec.x + rx[envobjs.arr[i].rot] * tile_size,
-                                 envobjs.arr[i].rec.y + ry[envobjs.arr[i].rot] * tile_size,
-                                 tile_size, tile_size},
-                     (Vector2){0, 0}, envobjs.arr[i].rot * 90, WHITE);
+    for (int my = 0; my < map_max_height; my++) {
+      for (int mx = 0; mx < map_max_width; mx++) {
+        DrawTexturePro(map.arr[mx][my].texture, (Rectangle){0, 0, tile_size, tile_size},
+                       (Rectangle){map.arr[mx][my].pos.x + rx[map.arr[mx][my].rot] * tile_size,
+                                   map.arr[mx][my].pos.y + ry[map.arr[mx][my].rot] * tile_size,
+                                   tile_size, tile_size},
+                       (Vector2){0, 0}, map.arr[mx][my].rot * 90, WHITE);
+      }
     }
     DrawRectangleRec((Rectangle){player.pos.x, player.pos.y, player.width, player.height},
                      (Color){0, 0, 0, 255});
